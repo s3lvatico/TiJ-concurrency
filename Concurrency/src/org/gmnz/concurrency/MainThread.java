60cf940102eef6eeda64a175a91eba3eb280389e
s3lvatico <s3lvatico@gmail.com>
2013-04-26 09:38:54 +0200

    Mi sono scordato due file....

---
+++ Concurrency/src/org/gmnz/concurrency/MainThread.java
@@ -0,0 +1,10 @@
+package org.gmnz.concurrency;
+
+import org.gmnz.concurrency.task.LiftOff;
+
+public class MainThread {
+	public static void main(String[] args) {
+		LiftOff l = new LiftOff();
+		l.run();
+	}
+}
