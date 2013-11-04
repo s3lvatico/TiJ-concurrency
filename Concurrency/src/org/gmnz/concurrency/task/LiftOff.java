4e4712c4443b5985c06d0411a1a7aed6ebfe1f2d
Simone Monotti <smonotti@solgenia.com>
2013-11-04 18:01:03 +0100

    Altra pennellata

--- Concurrency/src/org/gmnz/concurrency/task/LiftOff.java
+++ Concurrency/src/org/gmnz/concurrency/task/LiftOff.java
@@ -1,55 +1,66 @@
 package org.gmnz.concurrency.task;
 
+
 public class LiftOff implements Runnable {
 
-	private static int taskCount = 0;
-	/**
-	 * Serve a distinguere le varie istanze. Dichiarato final perchÈ una volta
-	 * inizializzato non serve cambiarlo.
-	 */
-	private final int id = ++taskCount;
-	protected int countDown = 10;
+   /**
+    * Contatore del numero di istanze.
+    */
+   private static int taskCount = 0;
+   /**
+    * Identificativo dell'istanza. Dichiarato final perch√© una volta
+    * inizializzato non serve cambiarlo.
+    */
+   private final int id = ++taskCount;
+   protected int countDown = 10;
 
-	public LiftOff() {
-	}
 
-	public LiftOff(int countDown) {
-		this.countDown = countDown;
-	}
 
-	public String status() {
-		return "#" + id + "(" + (countDown > 0 ? countDown : "Lift off!") + "). ";
-	}
+   public LiftOff() {}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Runnable#run()
-	 * 
-	 * Tipicamente questo metodo ha un qualche tipo di loop che si ripete finchÈ
-	 * il compito ("task") non Ë pi˘ necessario. Occorre quindi stabilire una
-	 * condizione secondo la quale si esca dal loop (la pi˘ semplice opzione Ë
-	 * quella di un enunciato "return" all'interno del metodo run() ). Spesso
-	 * accade che il metodo run() sia implementato sotto forma di loop infinito;
-	 * questo significa che, salvo i fattori che causano l'interruzione del
-	 * metodo run(), questo continuer‡ la sua esecuzione indefinitamente.
-	 */
-	@Override
-	public void run() {
-		while (countDown-- > 0) {
-			System.out.print(status());
-			/*
-			 * La chiamata a yield() Ë un suggerimento al thread scheduler. Il
-			 * thread scheduler Ë quella parte del meccanismo di threading di
-			 * Java che sposta la CPU da un thread all'altro. Questo
-			 * "suggerimento" dice
-			 * "ho completato le parti importanti del mio ciclo e questo Ë un buon momento per spostarsi su un altro thread se necessario"
-			 * . E' totalmente opzionale, ma Ë usato qui perchÈ tende a produrre
-			 * un output pi˘ interessante nei vari esempi.
-			 */
-			Thread.yield();
-		}
-	}
 
-	
+
+   public LiftOff(int countDown) {
+      this.countDown = countDown;
+   }
+
+
+
+   public String status() {
+      return "#" + id + "(" + (countDown > 0 ? countDown : "Lift off!") + ")   ";
+   }
+
+
+
+   /*
+    * (non-Javadoc)
+    *
+    * @see java.lang.Runnable#run()
+    *
+    * Tipicamente questo metodo ha un qualche tipo di loop che si ripete finch√©
+    * il compito ("task") non √® stato completato. Occorre quindi stabilire una
+    * condizione secondo la quale si esca dal loop (la pi√π semplice opzione √®
+    * quella di un enunciato "return" all'interno del metodo run() ). Spesso
+    * accade che il metodo run() sia implementato sotto forma di loop infinito;
+    * questo significa che, salvo i fattori che causano l'interruzione del
+    * metodo run(), questo continuer√† la sua esecuzione indefinitamente.
+    */
+   @Override
+   public void run() {
+      while (countDown-- > 0) {
+         System.out.print(status());
+         /*
+          * La chiamata a yield() √® un suggerimento al thread scheduler. Il
+          * thread scheduler √® quella parte del meccanismo di threading di
+          * Java che sposta la CPU da un thread all'altro. Questo
+          * "suggerimento" dice "ho completato le parti importanti del mio
+          * ciclo, e questo √® un buon momento per spostarsi su un altro thread,
+          * se necessario". E' totalmente opzionale, ma √® usato qui perch√©
+          * tende a produrre un output pi√π interessante nei vari esempi.
+          */
+         Thread.yield();
+      }
+   }
+
+
 }
